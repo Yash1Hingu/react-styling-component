7cfa0f2d2bd9c3181863a7af7c6fa137ce3fa2fa
Yash1Hingu <yash23hingu@gmail.com>
2023-05-11 22:24:08 +0530

    add x button

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -11,8 +11,9 @@ const CourseGoalItem = props => {
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
+    <li className="goal-item">
       {props.children}
+      <button onClick={deleteHandler}>X</button>
     </li>
   );
 };
